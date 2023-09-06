f28c71af1137d558891842f20d5f3afa35b4c9ba
Sourave halder <souravh093@gmail.com>
2023-09-06 15:22:42 +0600

    frontend some change

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -19,7 +19,6 @@ const Home = () => {
       <AboutUs />
       <TopCompany />
       <UserTestimonials />
-      <PreferenceJobs />
       <WorkThrough />
       <FrequentlyAskQuestion />
     </div>
